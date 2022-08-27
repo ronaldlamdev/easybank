28c6827ff9f43563a0587f2f2e6c6c8e9488b7b0
milessteamaccount <ronaldlamdev@gmail.com>
2022-08-26 19:05:37 -0500

    setup html structure of about component

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -6,8 +6,71 @@ import API from '../assets/icon-api.svg';
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className='w-full h-screen bg-[#f3f4f6]'>
+      <div className='max-w-[1680px] h-full mx-auto px-8 flex flex-col justify-center'>
 
+        {/* Upper About Text */}
+        <div>
+
+          <div>
+            <h1>Why Choose Easybank?</h1>
+            <p>
+              We leverage Open Banking to turn your bank account
+              into your financial hub. Control your finances like
+              never before. 
+            </p>
+          </div>
+          
+        </div>
+
+        {/* Lower About Text */}
+        <div>
+
+          {/* First column */}
+          <div>
+            <img src={Banking}/>
+            <h3>Online Banking</h3>
+            <p>
+              Our modern web and mobile applications allow
+              you to keep track of your finances wherever you 
+              are in the world.
+            </p>
+          </div>
+
+          {/* Second column */}
+          <div>
+            <img src={Budgeting}/>
+            <h3>Simple Budgeting</h3>
+            <p>
+              See exactly where your money goes each month. Receive 
+              notifications when you're close to hitting your limits.
+            </p>
+          </div>
+
+          {/* Third column */}
+          <div>
+            <img src={Onboarding}/>
+            <h3>Fast Onboarding</h3>
+            <p>
+              We don't do branches. Open your account in minutes
+              online and start taking control of your finances 
+              right away. 
+            </p>
+          </div>
+
+          {/* Open API */}
+          <div>
+            <img src={API}/>
+            <h3>Open API</h3>
+            <p>
+              Manage your savings, investments, pension, and
+              much more from one account. Tracking your money 
+              has never been easier. 
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import HomeImg from '../assets/image-mockups.png';
+
+const HomePage = () => {
+  return (
+    <div className='bg-[#fafafa] w-full h-fit'>
+
+      {/* Grid container */}
+      <div className='home-grid grid grid-cols-2'>
+        
+        {/* Home text */}
+        <div className='home-info flex flex-col justify-center max-w-[650px] w-full mx-auto px-8'>
+
+          <div className='text-6xl text-[#2d314d]'>
+            <h1>Next generation</h1>
+            <h1>digital banking</h1>
+          </div>
+
+          <p className='text-[#9698a6] text-xl py-12'>
+            Take your financial life online. Your Easybank account
+            will be a one-stop-shop for spending, saving, budgeting,
+            investing, and much more.
+          </p>
+
+          <button className='w-[180px]'>Request Invite</button>
+
+        </div>
+
+        {/* Home image */}
+
+        <div className='right relative z-10'>
+          <img className='w-full' src={HomeImg}/>
+        </div>
+        
+
+      </div>
+
+    </div>
+  )
+}
+
+export default HomePage
